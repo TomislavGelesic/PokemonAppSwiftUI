ccf4fa1f210adde9855cfb457fd16f619d669b65
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2021-07-29 14:27:55 +0200

    init

---
+++ Podfile
@@ -0,0 +1,12 @@
+
+
+platform:ios, '13.0'
+
+
+target 'PokemonAppSwiftUI' do
+
+    use_frameworks!
+
+    pod 'Kingfisher'
+    pod 'Alamofire'
+end
--- PokemonAppSwiftUI/Common/Views/PokemonAppSwiftUIApp.swift
+++ PokemonAppSwiftUI/Common/Views/PokemonAppSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PokemonAppSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
