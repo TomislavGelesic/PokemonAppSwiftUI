493c86544820a55e0d28e7c3249adcdf63fe854c
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2021-07-29 14:03:55 +0200

    Initial Commit

---
+++ PokemonAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokemonAppSwiftUI
+//
+//  Created by Tomislav Gelesic on 29.07.2021..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokemonAppSwiftUI/PokemonAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppSwiftUIApp.swift
+//  PokemonAppSwiftUI
+//
+//  Created by Tomislav Gelesic on 29.07.2021..
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
